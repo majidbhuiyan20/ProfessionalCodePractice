b0070e92675cbca236df57c57d6dd435cafd097d
majidbhuiyan20 <majid.bhuiyan2011@gmail.com>
2022-11-29 22:24:51 +0600

    Step 5 Completed

--- StudentList.java
+++ StudentList.java
@@ -10,7 +10,7 @@ public class StudentList
 		return line;
 	}
 	public  static BufferedWriter getFileBufferedWriter() throws Exception{
-		return new BufferedWriter(new FileWriter("students.txt", true));
+		return new BufferedWriter(new FileWriter(Constants.STUDENTS_FILE_NAME , true));
 	}
 
 
@@ -18,19 +18,19 @@ public class StudentList
 	{
 		if(args.length!=1)
 		{
-			System.err.println("Invalid number of argument");
-			System.err.println("Exiting Program.");
+			System.err.println(Constants.MSG_INVALID_NUMBER_OF_ARGUMENTS);
+			System.err.println(Constants.MSG_EXITING_PROGRAM);
 			System.exit(1);
 		}
 
 //		Check arguments
-		if(args[0].equals("a"))
+		if(args[0].equals(Constants.ARG_LIST_DATA))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println(Constants.MSG_LOADING_DATA);			
 			try
 			{
 			String line = getlineFromFile();
-			String students[] = line.split(", ");
+			String students[] = line.split(Constants.WORDS_SPLIT_REGEX);
 			for(String student : students) { System.out.println(student);
 			}
 			}
@@ -39,17 +39,17 @@ public class StudentList
 			{
 
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(Constants.MSG_LOADED_DATA);
 		}
 
-		else if(args[0].equals("r")) 
+		else if(args[0].equals(Constants.ARG_SHOW_RANDOM_DATA)) 
 		{
-			System.out.println("Loading data ...");			
+			System.out.println(Constants.MSG_LOADING_DATA);			
 			try
 			{
 			String line = getlineFromFile();
 
-			String students[] = line.split(", ");
+			String students[] = line.split(Constants.WORDS_SPLIT_REGEX);
 			Random random = new Random();
 				int index = random.nextInt(students.length);
 					System.out.println(students[index]);
@@ -58,20 +58,20 @@ public class StudentList
 			{
 
 			}
-			System.out.println("Data Loaded.");			
+			System.out.println(Constants.MSG_LOADED_DATA);			
 		}
-		else if(args[0].contains("+"))
+		else if(args[0].contains(Constants.ARG_ADD_DATA))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println(Constants.MSG_LOADING_DATA);			
 			try
 			{
 			BufferedWriter bufferedWriter = getFileBufferedWriter(); 	
 			String new_data  = args[0].substring(1);
 	        Date date = new Date();
-	        String date_format_pattern  = "dd-mm-yyyy-hh:mm:ss a";
+	        String date_format_pattern  = Constants.DATE_FORMAT_PATTERN;
 	        DateFormat dateFormat = new SimpleDateFormat(date_format_pattern );
 	        String fd= dateFormat.format(d);
-			bufferedWriter.write(", "+new_data+"\nList last updated on "+formate_date);
+			bufferedWriter.write(Constants.WORDS_SPLIT_REGEX+new_data+Constants.MSG_DATA_UPDATED+formate_date);
 			bufferedWriter.close();
 			}
 			catch (Exception e)
@@ -79,16 +79,16 @@ public class StudentList
 				
 			}
 							
-			System.out.println("Data Loaded.");	
+			System.out.println(Constants.MSG_LOADED_DATA);	
 		}
-		else if(args[0].contains("?")) 
+		else if(args[0].contains(Constants.ARG_FIND_DATA)) 
 		{
-			System.out.println("Loading data ...");			
+			System.out.println(Constants.MSG_LOADING_DATA);
 			try
 			{
 				
 			String line - getlineFromFile();
-			String students[] = line.split(",");
+			String students[] = line.split(Constants.WORDS_SPLIT_REGEX);
 			boolean done = false;
 			String new_data  = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++)
@@ -97,7 +97,7 @@ public class StudentList
 
 
 				{
-					System.out.println("We found it!");
+					System.out.println(Constants.MSG_DATA_FOUND);
 						done=true;
 				}
 			}
@@ -106,17 +106,14 @@ public class StudentList
 			{
 
 			}
-			System.out.println("Data Loaded.");				
+			System.out.println(Constants.MSG_LOADED_DATA);
 		}
-		else if(args[0].contains("c")) 
+		else if(args[0].contains(Constants.ARG_COUNT_WORDS))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println(Constants.MSG_LOADING_DATA);
 			try
 			{
-			BufferedReader bufferedReader = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String line = bufferedReader.readLine();
+			String line = getlineFromFile();
 			char students[] = line.toCharArray();
 			boolean in_word = false;
 			int count=0;
@@ -130,16 +127,16 @@ public class StudentList
 					}
 				}
 			}
-			System.out.println(count +" word(s) found ");
+			System.out.println(count +Constants.MSG_WORDS_FOUND);
 			}
 			catch (Exception e){}
-			System.out.println("Data Loaded.");				
+			System.out.println(Constants.MSG_LOADED_DATA);
 		}
 
 		else
 		{
-			System.err.println("Invalid number of argument");
-			System.err.println("Exiting Program.");
+			System.err.println(Constants.MSG_INVALID_ARGUMENTS);
+			System.err.println(Constants.MSG_EXITING_PROGRAM);
 			System.exit(2);
 		}
 	}
